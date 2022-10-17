beb706abea7560fb5d1b167a3b56ea6c1cc1841f
DocSkullka <docskullka@gmail.com>
2022-10-17 05:22:21 +0300

    init: finish task

---
+++ src/queue.js
@@ -0,0 +1,47 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+class Queue {
+  constructor() {
+    this.first = null
+    this.last = null
+  }
+
+  getUnderlyingList() {
+    return this.first
+  }
+
+  enqueue(value) {
+    const node = new ListNode(value);
+
+    if (!this.first) {
+      this.first = node;
+      this.last = this.first
+    } else {
+      this.last.next = node
+      this.last = node
+    }
+  }
+
+  dequeue() {
+    const result = this.first.value;
+    this.first = this.first.next;
+    return result
+  }
+}
+
+module.exports = {
+  Queue
+};
---
+++ src/stack.js
@@ -0,0 +1,37 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Implement the Stack with a given interface via array.
+ *
+ * @example
+ * const stack = new Stack();
+ *
+ * stack.push(1); // adds the element to the stack
+ * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ * stack.pop(); // returns the top element from stack and deletes it, returns 1
+ * stack.pop(); // undefined
+ *
+ */
+class Stack {
+  constructor() {
+    this.value = []
+  }
+
+  push(element) {
+    this.value = [...this.value, element]
+  }
+
+  pop() {
+    const result = this.value[this.value.length - 1]
+    this.value.length = this.value.length - 1
+    return result;
+  }
+
+  peek() {
+    return this.value[this.value.length - 1];
+  }
+}
+
+module.exports = {
+  Stack
+};
